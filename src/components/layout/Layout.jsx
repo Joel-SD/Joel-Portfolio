203bcf698394f4293ca7f03a37e2fb37190d24e6
Joel-SD <joelcarrasco.sd@gmail.com>
2025-10-28 21:52:43 -0500

    feat: add scroll progress bar component for enhanced user experience

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import ScrollProgressBar from '../common/ScrollProgressBar';
 import { LanguageProvider } from '../../context/LanguageContext';
 
 const Layout = ({ children }) => {
   return (
     <LanguageProvider>
       <div className="relative w-full">
+        <ScrollProgressBar />
         <Header />
         <main className="flex flex-col items-center gap-14 pt-20 pb-12 w-full">
           {children}
